48ef1e83bed80bd0a9174e94fc3a84c955cd53de
Hedgehog <i@ezhik.me>
2025-03-07 17:13:36 +0000

    Update

--- app/pip-pwa.js
+++ app/pip-pwa.js
@@ -6,27 +6,6 @@ import { h } from "./lib/userscript.js";
   document.head.append(link);
 }
 function init(el) {
-  const metaIcon = h("link", {
-    rel: "apple-touch-icon",
-    href: "/favicon.png"
-  });
-  document.head.append(metaIcon);
-  const metaWebAppCapable = h("meta", {
-    name: "apple-mobile-web-app-capable",
-    content: "yes"
-  });
-  document.head.append(metaWebAppCapable);
-  const appName = "Play in PiP";
-  const metaWebAppTitle = h("meta", {
-    name: "apple-mobile-web-app-title",
-    content: appName
-  });
-  document.head.append(metaWebAppTitle);
-  const metaApplicationName = h("meta", {
-    name: "application-name",
-    content: appName
-  });
-  document.head.append(metaApplicationName);
   const input = h("input", { type: "file", accept: "video/*" });
   el.append(input);
   const videoContainer = h("div", { className: "video-container" });
@@ -49,7 +28,7 @@ function init(el) {
     video.src = url;
     video.play();
     try {
-      video.controls = true;
+      video.requestPictureInPicture();
     } catch (e) {
       console.error(e);
     }
